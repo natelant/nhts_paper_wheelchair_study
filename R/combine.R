08f3f87b619b8befe249e2908228af9c09b62c26
Nathan John Lant <nlant@et.byu.edu>
2020-01-10 15:04:37 -0700

    finish combine data script. delete the cleaning.r

--- R/combine.R
+++ R/combine.R
@@ -1,4 +1,6 @@
-## Combine
+## Combine persons, households, and trips into one file.
+## this is a list of all of the trips with person and house data attached
+## also includes trips not taken
 
 # for reference to the NHTS codebook https://nhts.ornl.gov/assets/codebook_v1.1.pdf
 
@@ -6,7 +8,7 @@
 # the goal is to do the following
 # 1. merge persons, households, and trips, only including the necessary variables
 # 2. create new variables (Ability, Worker, Age, etc.)
-# 3. create the grid to count number of trips per purpose.
+
 
 # install.packages("devtools")
 # devtools::install_github("byu-transpolab/nhts2017")
@@ -18,22 +20,20 @@ library(tidyverse)
 # Join persons, households, and trips together into one data set.
 # select only the relevant columns (easy to add more variables later)
 # then write into an .rds file and store in data folder
+
+
 my_nhts <- nhts_persons %>%
   select(houseid, personid, w_chair, w_mtrchr, medcond6, medcond, r_age, wkftpt, wtperfin) %>%
   left_join(nhts_households %>% 
               # I only need a couple variables from the household file
               select(houseid, hhfaminc, hhstate), 
             by = "houseid") %>%
-  # because households doesn't have a personid, i join persons-households first, then I can 
-  # create the hhpersonid to join with trips.
-  # mutate(hhpersonid = paste(houseid, personid, sep = "-")) %>%
+  
   # now I can join with trips
   left_join(nhts_trips %>%
               # mutate first, so that i can exclude the houseid and personid when I join with persons
               mutate(
-                # hhpersonid = paste(houseid, personid, sep = "-"),
                 trpmiles = as.double(trpmiles),
-                # trip_ID = paste(hhpersonid, tdtrpnum, sep = "-")
                      ) %>%
               select(houseid, personid, tdtrpnum, trptrans, trvlcmin, trpmiles, psgr_flg, trippurp),
             # maybe i will need to filter the trip data (miles > 0, exclude the -9s and -1s and stuff)
@@ -95,62 +95,11 @@ my_nhts <- nhts_persons %>%
     Mode = fct_relevel(Mode, "Car (Driver)", "Car (Passenger)", "Van (Driver)", 
                        "Van (Passenger)", "Walk", "Local Transit", "Paratransit", 
                        "Taxi (Including Uber/Lyft)", "Other")
-  ) %>% 
+  ) %>%
   
-  
-  filter(
-    # by filtering out Ability "NA", I exclude 1931 / 264233 respondents (0.73%)
-    Ability != "NA",
-    Income != "NA",
-    Worker != "NA",
-  ) 
-
-#--------------------------------------------------------------------------------------------------
-## BUILDING THE GRID
-# This data will contain all trips (all households, persons, etc.)
-# whereas in the example Dr. Macfarlane sent me, he filtered the households by urban size, 
-# msa size, travel day, etc.
-
-# First find all trips that belong to an hhpersonid, group them by purpose and count them
-my_trips <- nhts_trips %>% 
-  # the trips data file also has houseid and personid. first combine these into one id
-  mutate(hhpersonid = paste(houseid, personid, sep = "-")) %>%
-  # only use the trips from the hhpersonid in the persons_edited file
-  filter(hhpersonid %in% my_nhts$hhpersonid) %>%
-  group_by(hhpersonid, trippurp) %>%
-  # make a count of repeating person/trip purpose combinations. For example, how many times did
-  # 300...07-01 make an HBO trip.
-  tally() %>%
-  ungroup() %>%
-  # anything bigger than 10 trips will be counted as 10 trips.
-  mutate(n = ifelse(n > 10, 10, n))
+  write_rds("data/nhts_data.rds")
 
 
-# this grid has over 1 million rows. it has each purpose for each person
-my_grid <- expand_grid(
-  # uses only the unique hhpersonids found in the new_trips data
-  hhpersonid = unique(my_trips$hhpersonid), 
-  # uses only HBO, HBW, HBSOCREC, NHB, and HBSOP
-  trippurp = unique(my_trips$trippurp)
-) %>%
-  filter(trippurp != "-9")
-
-
-# combine the grid and the trips
-# this new data set has over 1 million rows. each row is a trip purpose per person and has all of the 
-# person data attached with it.
-# n is the number of trips for a specific person and purpose
-try <- my_trips %>% 
-  right_join(my_grid) %>%
-  # if no trips were taken show they took 0 trips
-  mutate(n = ifelse(is.na(n), 0, n)) %>%
-  # add all of the person data (including Ability, Age, Worker, Income groups etc.)
-  # this data set is over 4 million rows deep.
-  # it doubles each person and trip purpose combo because there are two trip miles values per trip
-  right_join(my_nhts, "hhpersonid")
-  
-  
-  write_rds("data/combined.rds")
 
 
 
