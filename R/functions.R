40fe36847c19e69a42063fa17d5fb60ddeec5b09
Nathan John Lant <nlant@et.byu.edu>
2020-02-17 18:29:50 -0700

    change function name to add_tours. clean code. add DAP column

--- R/functions.R
+++ R/functions.R
@@ -10,8 +10,11 @@
 # library(lubridate)
 
 # make data easier to test
-# trips <- nhts_trips %>%
-  # filter(houseid %in% c("40794204", "40794233", "40794241"))
+ trips <- nhts_trips %>%
+   filter(houseid %in% c("40793049", "40793216", "40793969", "40793969"))
+ 
+ trips <- nhts_trips %>%
+   filter(houseid == "40793216")
 
 
 # Necessary fields include houseid, personid, strttime, endtime, whyfrom, whyto
@@ -25,6 +28,8 @@ build_activities <- function(trips) {
     # gather times
     gather(strtend, time, strttime, endtime) %>%
     group_by(houseid, personid) %>%
+    # there have been errors where a time stamp is wrongly labeled and
+    # wrongly arranged here...and again on line 57
     arrange(time, .by_group = TRUE) %>%
     mutate(
       event = ifelse(strtend == "strttime", "depart", "arrive"),
@@ -36,8 +41,7 @@ build_activities <- function(trips) {
     # take the starting point of each person
     slice(1) %>%
     # creates a starting time for each person at 4:00:00 am
-    transmute(activity = as.character(whyfrom), #%>%
-              #paste("S", sep = "-"),
+    transmute(activity = as.character(whyfrom),
               time = as_datetime("2017-10-10 4:00:00"),
               event = "arrive")
   
@@ -64,8 +68,9 @@ build_activities <- function(trips) {
 
 # This function takes the activities list and numbers and classifies the tours each 
 # person takes during the day
+# output is a list of activities with a tour classification column
 
-build_tours <- function(activities) {
+add_tours <- function(activities) {
   
   # create columns for tour count 
   activity_list <- activities %>% 
@@ -86,20 +91,37 @@ build_tours <- function(activities) {
       summarise(tour_list = paste(activity, collapse = " ")) %>%
       mutate(tour_class = case_when(
                            str_detect(tour_list, "03") == T ~ "W",
-                           str_detect(tour_list, "04") == T ~ "W", 
-                           # I don't know if school or work will take priority
+                           str_detect(tour_list, "04") == T ~ "W",
                            str_detect(tour_list, "08") == T ~ "S",
                            str_detect(tour_list, "01") == T ~ "home",
                            TRUE ~ "NM"
                            )) %>%
       # filter out the tours that are "home" because technically they aren't even tours.
-      filter(tour_class != "home")
+      filter(tour_class != "home"
+             # when tour_count = 0, the person started at somewhere other than home
+             # and they will be cut out of the data
+             # tour_count != 0)
+      )
+      # collapse by tour_class add column for w1 and w2
+    daps <- tour_list %>%
+      summarise(tours_row = paste(tour_class, collapse = "-")) %>%
+      mutate(DAP = case_when(
+        str_detect(tours_row, "W-W") == T ~ "W_2",
+        str_detect(tours_row, "W") == T ~ "W_1",
+        str_detect(tours_row, "S-S") == T ~ "S_2",
+        str_detect(tours_row, "S") == T ~ "S_1",
+        TRUE ~ "NM"
+        # still leaves the question, what about a tour like H-W-W-W == W-2?
+        # this would need to be figured out somewhere else...
+      ))
       
       # join back onto activites to create the classification column
     activity_list %>%
       left_join(tour_list, by = c("houseid", "personid", "tour_count")) %>%
-      # eliminate tour_list
-      select(-tour_list)
+      #include the dap column
+      left_join(daps,  by = c("houseid", "personid")) %>%
+      # eliminate tour_list and tours_row
+       select(-tour_list, -tours_row)
       
   
 }
